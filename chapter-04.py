99402be8d050493e366ff8333a322d5a14b3334a
Chen Xiangtuo <xiangtuo.chen@ecp.fr>
2018-05-18 19:41:12 +0200

    first mastering scikit learn

---
+++ chapter-04.py
@@ -0,0 +1,318 @@
+### From Linear Regression to Logistic Regression
+### Binary classification with logistic regression
+
+################# Sample 1: Spam filtering #################
+import pandas as pd
+df = pd.read_csv('SMSSpamCollection', delimiter='\t', header=None)
+print(df.head())
+print('Number of spam messages:', df[df[0] == 'spam'][0].count())
+print('Number of ham messages:', df[df[0] == 'ham'][0].count())
+
+################# Sample 2 #################
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.cross_validation import train_test_split
+
+df = pd.read_csv('SMSSpamCollection', delimiter='\t', header=None)
+X_train_raw, X_test_raw, y_train, y_test = train_test_split(df[1], df[0])
+#X_train_raw, X_test_raw, y_train, y_test = train_test_split(df['message'], df['label'])
+vectorizer = TfidfVectorizer()
+X_train = vectorizer.fit_transform(X_train_raw)
+X_test = vectorizer.transform(X_test_raw)
+classifier = LogisticRegression()
+classifier.fit(X_train, y_train)
+predictions = classifier.predict(X_test)
+
+for i in [1,2,3,4,5] :
+    print('Prediction: %s' % (predictions[i]))
+
+
+### Binary classification performance metrics
+################# Sample 3 #################
+
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
+y_test = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+y_pred = [0, 1, 0, 0, 0, 0, 0, 1, 1, 1]
+confusion_matrix = confusion_matrix(y_test, y_pred)
+print(confusion_matrix)
+plt.matshow(confusion_matrix)
+plt.title('Confusion matrix')
+plt.colorbar()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
+
+### Accuracy
+################# Sample 4 #################
+
+from sklearn.metrics import accuracy_score
+y_pred, y_true = [0, 1, 1, 0], [1, 1, 1, 1]
+print(accuracy_score(y_true, y_pred))
+
+
+################# Sample: Evaluating the SMS Classifier #################
+
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.cross_validation import train_test_split, cross_val_score
+
+df = pd.read_csv('sms.csv')
+X_train_raw, X_test_raw, y_train, y_test = train_test_split(df['message'], df['label'])
+vectorizer = TfidfVectorizer()
+X_train = vectorizer.fit_transform(X_train_raw)
+X_test = vectorizer.transform(X_test_raw)
+classifier = LogisticRegression()
+classifier.fit(X_train, y_train)
+scores = cross_val_score(classifier, X_train, y_train, cv=5)
+print(np.mean(scores), scores)
+
+### Precision and recall
+################# Sample 6 #################
+
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.cross_validation import train_test_split, cross_val_score
+
+df = pd.read_csv('sms.csv')
+X_train_raw, X_test_raw, y_train, y_test = train_test_split(df['message'], df['label'])
+vectorizer = TfidfVectorizer()
+X_train = vectorizer.fit_transform(X_train_raw)
+X_test = vectorizer.transform(X_test_raw)
+classifier = LogisticRegression()
+classifier.fit(X_train, y_train)
+precisions = cross_val_score(classifier, X_train, y_train, cv=5, scoring='precision')
+print('Precision', np.mean(precisions), precisions)
+recalls = cross_val_score(classifier, X_train, y_train, cv=5, scoring='recall')
+print('Recall', np.mean(recalls), recalls)
+
+################# Sample: F1 Score #################
+
+f1s = cross_val_score(classifier, X_train, y_train, cv=5, scoring='f1')
+print('F1', np.mean(f1s), f1s)
+
+
+################# Sample: ROC AUC #################
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.cross_validation import train_test_split, cross_val_score
+from sklearn.metrics import roc_curve, auc
+
+df = pd.read_csv('sms.csv')
+X_train_raw, X_test_raw, y_train, y_test = train_test_split(df['message'], df['label'])
+vectorizer = TfidfVectorizer()
+X_train = vectorizer.fit_transform(X_train_raw)
+X_test = vectorizer.transform(X_test_raw)
+classifier = LogisticRegression()
+classifier.fit(X_train, y_train)
+predictions = classifier.predict_proba(X_test)
+false_positive_rate, recall, thresholds = roc_curve(y_test, predictions[:, 1])
+roc_auc = auc(false_positive_rate, recall)
+plt.title('Receiver Operating Characteristic')
+plt.plot(false_positive_rate, recall, 'b', label='AUC = %0.2f' % roc_auc)
+plt.legend(loc='lower right')
+plt.plot([0, 1], [0, 1], 'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.0])
+plt.ylabel('Recall')
+plt.xlabel('Fall-out')
+plt.show()
+
+
+################# Sample 8 #################
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.metrics import roc_curve, auc
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cross_validation import train_test_split
+
+df = pd.read_csv('sms.csv')
+X_train_raw, X_test_raw, y_train, y_test = train_test_split(df['message'], df['label'])
+vectorizer = TfidfVectorizer()
+X_train = vectorizer.fit_transform(X_train_raw)
+X_test = vectorizer.transform(X_test_raw)
+classifier = LogisticRegression().fit(X_train, y_train)
+clf = LogisticRegression()
+clf.fit(X_train, y_train)
+predictions = clf.predict_proba(X_test)
+false_positive_rate, recall, threhsolds = roc_curve(y_test, predictions[:, 1])
+roc_auc = auc(false_positive_rate, recall)
+plt.title('Receiver Operating Characteristic')
+plt.plot(false_positive_rate, recall, 'b', label='AUC = %0.2f' % roc_auc)
+plt.legend(loc='lower right')
+plt.plot([0, 1], [0, 1], 'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.0])
+plt.ylabel('Recall')
+plt.xlabel('Fall-out')
+plt.show()
+
+
+################# Sample 9 #################
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import precision_score, recall_score, accuracy_score
+
+pipeline = Pipeline([
+    ('vect', TfidfVectorizer(stop_words='english')),
+    ('clf', LogisticRegression())
+])
+parameters = {
+    'vect__max_df': (0.25, 0.5, 0.75),
+    'vect__stop_words': ('english', None),
+    'vect__max_features': (2500, 5000, 10000, None),
+    'vect__ngram_range': ((1, 1), (1, 2)),
+    'vect__use_idf': (True, False),
+    'vect__norm': ('l1', 'l2'),
+    'clf__penalty': ('l1', 'l2'),
+    'clf__C': (0.01, 0.1, 1, 10),
+}
+
+if __name__ == "__main__":
+    grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1, verbose=1, scoring='accuracy', cv=3)
+    df = pd.read_csv('sms.csv')
+    X, y, = df['message'], df['label']
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    grid_search.fit(X_train, y_train)
+    print('Best score: %0.3f' % grid_search.best_score_)
+    print('Best parameters set:')
+    best_parameters = grid_search.best_estimator_.get_params()
+    for param_name in sorted(parameters.keys()):
+        print('\t%s: %r' % (param_name, best_parameters[param_name]))
+    predictions = grid_search.predict(X_test)
+    print('Accuracy:', accuracy_score(y_test, predictions))
+    print('Precision:', precision_score(y_test, predictions))
+    print('Recall:', recall_score(y_test, predictions))
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import precision_score, recall_score, accuracy_score
+
+pipeline = Pipeline([
+    ('vect', TfidfVectorizer(stop_words='english')),
+    ('clf', LogisticRegression())
+])
+parameters = {
+    'vect__max_df': (0.25, 0.5, 0.75),
+    'vect__stop_words': ('english', None),
+    'vect__max_features': (2500, 5000, 10000, None),
+    'vect__ngram_range': ((1, 1), (1, 2)),
+    'vect__use_idf': (True, False),
+    'vect__norm': ('l1', 'l2'),
+    'clf__penalty': ('l1', 'l2'),
+    'clf__C': (0.01, 0.1, 1, 10),
+}
+
+if __name__ == "__main__":
+    grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1, verbose=1, scoring='accuracy', cv=3)
+    df = pd.read_csv('sms.csv')
+    X, y, = df['message'], df['label']
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    grid_search.fit(X_train, y_train)
+    print('Best score: %0.3f' % grid_search.best_score_)
+    print('Best parameters set:')
+    best_parameters = grid_search.best_estimator_.get_params()
+    for param_name in sorted(parameters.keys()):
+        print('\t%s: %r' % (param_name, best_parameters[param_name]))
+    predictions = grid_search.predict(X_test)
+    print('Accuracy:', accuracy_score(y_test, predictions))
+    print('Precision:', precision_score(y_test, predictions))
+    print('Recall:', recall_score(y_test, predictions))
+
+
+################# Sample 9 #################
+
+import pandas as pd
+df = pd.read_csv('train.tsv', header=0, delimiter='\t')
+print(df.count())
+print(df.head(10))
+print(df['Phrase'].head())
+print(df['Sentiment'].describe())
+print(df['Sentiment'].value_counts())
+print(df['Sentiment'].value_counts()/df['Sentiment'].count())
+
+
+################# Sample: Multi-Class Classification of Movie Review Sentiments #################
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from sklearn.pipeline import Pipeline
+from sklearn.grid_search import GridSearchCV
+
+def main():
+    pipeline = Pipeline([
+        ('vect', TfidfVectorizer(stop_words='english')),
+        ('clf', LogisticRegression())
+    ])
+    parameters = {
+        'vect__max_df': (0.25, 0.5),
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        'vect__use_idf': (True, False),
+        'clf__C': (0.1, 1, 10),
+    }
+    df = pd.read_csv('train.tsv', header=0, delimiter='\t')
+    X, y = df['Phrase'], df['Sentiment'].as_matrix()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.5)
+    grid_search = GridSearchCV(pipeline, parameters, n_jobs=3, verbose=1, scoring='accuracy')
+    grid_search.fit(X_train, y_train)
+    print('Best score: %0.3f' % grid_search.best_score_)
+    print('Best parameters set:')
+    best_parameters = grid_search.best_estimator_.get_params()
+    for param_name in sorted(parameters.keys()):
+        print('\t%s: %r' % (param_name, best_parameters[param_name]))
+
+if __name__ == '__main__':
+    main()
+
+
+################# Sample 11 #################
+# Multi-Class Classification Performance Metrics
+
+predictions = grid_search.predict(X_test)
+print('Accuracy:', accuracy_score(y_test, predictions))
+print('Confusion Matrix:', confusion_matrix(y_test, predictions))
+print('Classification Report:', classification_report(y_test, predictions))
+
+
+################# Sample 12 #################
+# Applying Multi-label Classification
+
+
+
+################# Sample 13 #################
+# Multi-Label Classification Performance Metrics
+
+import numpy as np
+from sklearn.metrics import hamming_loss, jaccard_similarity_score
+print(hamming_loss(np.array([[0.0, 1.0], [1.0, 1.0]]), np.array([[0.0, 1.0], [1.0, 1.0]])))
+print(hamming_loss(np.array([[0.0, 1.0], [1.0, 1.0]]), np.array([[1.0, 1.0], [1.0, 1.0]])))
+print(hamming_loss(np.array([[0.0, 1.0], [1.0, 1.0]]), np.array([[1.0, 1.0], [0.0, 1.0]])))
+print(jaccard_similarity_score(np.array([[0.0, 1.0], [1.0, 1.0]]), np.array([[0.0, 1.0], [1.0, 1.0]])))
+print(jaccard_similarity_score(np.array([[0.0, 1.0], [1.0, 1.0]]), np.array([[1.0, 1.0], [1.0, 1.0]])))
+print(jaccard_similarity_score(np.array([[0.0, 1.0], [1.0, 1.0]]), np.array([[1.0, 1.0], [0.0, 1.0]])))
